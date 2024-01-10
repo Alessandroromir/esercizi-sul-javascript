ae26c91a5e15e1e6c85240dad68ac69f4277233a
Alessandro Governale <samuele_governale@live.it>
2024-01-10 19:49:55 +0100

    feat(esercizi) added exercise25

---
+++ 25.js
@@ -0,0 +1,19 @@
+// Funzione per gestire l'evento mousemove
+
+function handleMouseMove(event) {
+
+    // Ottieni le coordinate della posizione del mouse
+
+    const mouseX = event.clientX;
+
+    const mouseY = event.clientY;
+
+    // Aggiorna il testo nell'elemento HTML
+
+    document.getElementById('coordinates').textContent =
+    `X: ${mouseX}, Y: ${mouseY}`;
+}
+
+// Aggiungi un ascoltatore per l'evento mousemove all'interno del documento
+
+document.addEventListener('mousemove', handleMouseMove);
